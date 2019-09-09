abafc0e89d284e7b03fb74bedc02f930a047d7b4
José Luis Silva <jlsilva1994@gmail.com>
2019-09-09 13:36:55 +0100

    remove tests folder

--- test/src/TestCase-00.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Created by miguel on 12-07-2019.
- * Copyright (c) 2019 Deeep. All rights reserved.
- */
-// Let Catch provide main():
-#define CATCH_CONFIG_MAIN
-
-#include "catch2/catch.hpp"
